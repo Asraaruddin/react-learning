b390c72215d63b93a0b4148e3f325cc6e0f08e40
AsrarUddin <asraruddin000@gmail.com>
2025-04-13 16:29:22 +0530

    Update UI and fix state handling in day-3 components

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './day-3/app.jsx' // ✅ import your Day 3 app
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
+import  'bootstrap' ;
 
 
 createRoot(document.getElementById('root')).render(
