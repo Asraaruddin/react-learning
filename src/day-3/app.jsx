e2595da0f1c0524e1f6706c16020a62ba87d69b1
AsrarUddin <asraruddin000@gmail.com>
2025-04-13 15:39:37 +0530

    Update greeting.jsx

--- src/day-3/app.jsx
+++ src/day-3/app.jsx
@@ -1,18 +1,11 @@
 // src/day-3/App.jsx
 import React from 'react'
-import Day3 from './day3.jsx'
+import UserGreeting from './day3.jsx'
 function App() {
     return (
-      <div style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-        textAlign: 'center'
-      }}>
-        <h1>Welcome to Day 3</h1>
-        <Day3 />
+      <div>
+   
+        <UserGreeting isLoggedIn={false} username="AsrarUddin" />
       </div>
     )
   }
--- src/day-3/day3.jsx
+++ src/day-3/day3.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-function Day3() {
-  return <p>This is Day 3 component!</p>
-}
 
-export default Day3
+
+
+
+function UserGreeting(props) {
+    const [username, setUsername] = useState('');
+    const [city, setCity] = useState('');
+const [isSubmited,setIsSubmitted] = useState(false);
+
+const handleSubmit= () =>{
+  setIsSubmitted(true);
+} 
+
+  return (   
+    
+    <div className='container-fluid mt-5  bg-dark text-white  '>
+         
+          <h1>Welcome !</h1>
+          <input type="text" 
+          placeholder='Enter your name'
+          className='form-control mb-2 w-25 '
+          value={username}
+          onChange={(e)=>setUsername(e.target.value)}
+          />
+            <select className='form-selct mb-3 w-25' 
+            value={city} 
+            onChange={(e)=>setCity(e.target.value)}
+            >
+                <option value="-1">Select your city</option>
+                <option value="chennai">chennai</option>
+                <option value="hyd">hyd</option>
+                <option value="delhi">delhi</option>
+            </select>
+            <br />
+            <button className='btn btn-primary ' onClick={handleSubmit}>
+              Submit
+            </button>
+            {isSubmited &&(
+              <p className='mt-4'>Hello! {username} from {city}</p>
+            )}
+            
+    </div>
+ 
+    )
+
+
+  }
+
+export default UserGreeting;
