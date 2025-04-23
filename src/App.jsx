36725a4429e2b6b32415b53f7c38c9c49cb30a6b
AsrarUddin <asraruddin000@gmail.com>
2025-04-23 22:26:49 +0530

    fakestoreapi

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,14 @@ import Footer from './Components/Footer';
 import TodoContainer from './Components/TodoContainer';
 import { Debounce } from './Components/debounce';
 import { Debouncedemo } from './Components/debouncedemo';
+import { FakestoreProducts } from './Components/fakestorapi';
 
 // import { ClassBinding } from './Components/class-bindinf';
 
 const App = () => {
   return (
     <div className="min-h-screen  flex items-center justify-center">
-    <Debouncedemo/>
+    <FakestoreProducts/>
     
   </div>
   );
