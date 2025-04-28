173b90f6751f8e6e1b664ec41dba490e56cb17c9
AsrarUddin <asraruddin000@gmail.com>
2025-04-28 23:01:14 +0530

    add toggle

--- src/Components/toggle2.jsx
+++ src/Components/toggle2.jsx
@@ -1,9 +1,15 @@
-import { useState } from "react"; 
+import { useEffect, useState } from "react"; 
 
 export function useToggle2(){
 
-    const [theme, setTheme] = useState('border-dark');
-    const [btnTheme, setbtnTheme] = useState('btn-dark');
+    const [theme, setTheme] = useState(()=>{
+        const savedTheme = localStorage.getItem("theme");
+        return savedTheme ? savedTheme : 'border-dark';
+    })
+    const [btnTheme, setbtnTheme] = useState(()=>{
+        const savedBtnTheme = localStorage.getItem("btnTheme");
+        return savedBtnTheme ? savedBtnTheme : 'btn-dark';
+    })
 
     function handleThemeChange(e){
         if(e.target.checked){
@@ -14,5 +20,12 @@ export function useToggle2(){
             setbtnTheme('btn-dark');
         }
     }
+useEffect(()=>{
+    localStorage.setItem("theme",theme);
+    localStorage.setItem("btnTheme",btnTheme);
+    },[theme,btnTheme]);
+
+    
+
     return {theme , btnTheme, handleThemeChange} 
 }
